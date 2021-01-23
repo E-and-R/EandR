81e36c7d91e8a76e9f16780d93902bc044469cfb
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2021-01-23 23:21:29 +0200

    Changed the node component which processes project details markup texts

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     const { createPage } = actions
   
     const blogPostTemplate = require.resolve(`./src/templates/blogTemplate.js`)
-  
+    const projectDetailsTemplate = require.resolve(`./src/templates/projectdetailstemplate.js`)
     const result = await graphql(`
       {
         allMarkdownRemark(
@@ -29,7 +29,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
         path: node.frontmatter.slug,
-        component: blogPostTemplate,
+        component: projectDetailsTemplate,
         context: {
           // additional data can be passed via context
           slug: node.frontmatter.slug,
