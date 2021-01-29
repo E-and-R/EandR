120efb06c78f52029c425723c9b975711bc9f7ba
junior <morabav@gmail.com>
2021-01-29 17:52:25 +0200

    updated themeprovider for consistency

---
+++ mui-root-wrapper.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './src/components/Theme'
+
+export default ({ element }) => (
+  <ThemeProvider theme={theme}>
+    {element}
+  </ThemeProvider>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link, graphql } from 'gatsby'
 import "../styles/styles.scss"
 import SEO from "../components/seo"
 import Header from "../components/header"
@@ -8,29 +7,22 @@ import LandingPageBanner from "../components/landingpagebanner"
 import { makeStyles } from '@material-ui/core/styles';
 import Theme from "../components/Theme"
 import { ThemeProvider } from '@material-ui/styles'
-import { Grid, Typography } from "@material-ui/core"
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    backgroundColor: theme.palette.background.paper,
+
+export default function IndexPage(){
+    return(    
+      <ThemeProvider theme={Theme}>
+           <div >
+            <SEO title="Home" />
+            <Header/>
+            <LandingPageBanner title={"Don't Waste Anymore\nLight-Bulb\nMoments"} subtitle="At Einstein and Robin we are in the business of assisting you maximise your light-bulb moments. Get in touch with us today and lets convert those ideas into results."/>
+            <Footer/>
+          </div>
+      </ThemeProvider>
+     
     
-  },
-}));
-
-const IndexPage = () => (
   
-    <ThemeProvider theme={Theme}>
-      <div >
-        <SEO title="Home" />
-        <Header/>
-        <LandingPageBanner title={"Don't Waste Anymore\nLight-Bulb\nMoments"} subtitle="At Einstein and Robin we are in the business of assisting you maximise your light-bulb moments. Get in touch with us today and lets convert those ideas into results."/>
-        <Footer/>
-      </div>
-    </ThemeProvider>
-  
-)
+    )
+}
 
-export default IndexPage
+
